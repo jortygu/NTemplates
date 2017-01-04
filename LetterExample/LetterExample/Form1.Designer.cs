e511adf6dcb1d9a32f771fe29768379db4a98618
Gonzalo Méndez <gonzalo.mendez@outlook.com>
2017-01-03 22:03:40 -0300

    Added Letter example

---
+++ LetterExample/LetterExample/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace LetterExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerateLetter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGenerateLetter
+            // 
+            this.btnGenerateLetter.Location = new System.Drawing.Point(119, 162);
+            this.btnGenerateLetter.Name = "btnGenerateLetter";
+            this.btnGenerateLetter.Size = new System.Drawing.Size(109, 23);
+            this.btnGenerateLetter.TabIndex = 0;
+            this.btnGenerateLetter.Text = "Generate letter";
+            this.btnGenerateLetter.UseVisualStyleBackColor = true;
+            this.btnGenerateLetter.Click += new System.EventHandler(this.btnGenerateLetter_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 261);
+            this.Controls.Add(this.btnGenerateLetter);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerateLetter;
+    }
+}
+
---
+++ LetterExample/LetterExample/Form1.cs
@@ -0,0 +1,66 @@
+﻿using NTemplates;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LetterExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerateLetter_Click(object sender, EventArgs e)
+        {
+            //First, we need an instance of the DocumentCreator class.
+            DocumentCreator dc = new DocumentCreator();
+
+            //Now, we need to pass the data we want in the report, to the document creator instance.
+            //We are going to "query" and bind the data in a single method, for simplicity.
+            AddDataForReport(dc);
+
+            //Finally, we will create a report, based on our template and save it to a location.
+            //I encourage you to explore other overloads of the function CreateDocument
+            dc.CreateDocument(@"../../Documents/letterTemplate.rtf", @"../../Documents/generatedLetter.rtf");
+        }
+
+
+        private void AddDataForReport(DocumentCreator dc)
+        {
+            /* Here we are going to add the data that we want to display. Typically it would come from a Database, a file, or a
+             * web service, but NTemplates doesn´t care about that, it´s up to you and your requirements.
+             * NOTE: NTemplates accepts input data in several formats.
+             *    - As ASP.Net data tables.
+             *    - As lists of POCOs.
+             *    - As variables.
+             * Here we are going to start by using simple variables.
+             * 
+             * PLEASE NOTE THAT VARIABLE NAMES ARE CASE SENSITIVE */
+
+            dc.AddString("title", "Mr.");
+
+            dc.AddString("name", "Magoo");
+
+            /* We could add a date in it´s native format and then format it as we wish  at the template itself. That´s the recommended way of doing
+             * it because it provides much more flexibility for the end users.
+             * In order to keep things simple we will add the date as string. I will introduce formatting functions in future 
+             * articles. You can explore the examples inclued with the NTemplates downloads. */            
+            dc.AddString("date", DateTime.Today.ToShortDateString());
+
+            dc.AddString("address", "secret for now, and will be revealed at the last moment");
+
+            dc.AddString("email","gmddmg@gmail.com");
+
+            dc.AddString("inviterName", "Gonzalo Méndez");
+        
+            dc.AddString("inviterCompany", "gmddmg@gmail.com");
+
+            dc.AddString("inviterUrl", "www.rocksolidlabs.com");
+
+            Image ourLogo = Image.FromFile(@"../../Documents/rocksolidlogo.JPG");
+            dc.AddImage("rocksolidlogo", ourLogo);
+        }
+    }
+}
