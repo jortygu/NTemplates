c6c7a2a4ec632ea6dd5ac2e1dd62124653d06d04
Harvin <harvintest@gmail.com>
2019-02-06 09:44:13 +0100

    System.Windows.Forms.RichTextBox sometimes Error Creating Window Handle

--- NTemplates/TextCleaner.cs
+++ NTemplates/TextCleaner.cs
@@ -6,25 +6,29 @@ using System.Collections.Generic;
 namespace NTemplates
 {
     internal static class TextCleaner
-    {        
+    {
         internal static string CleanText(string rtfText, string KeywordsRegExString)
         {
             Regex regex = CommonMethods.GetRegex(KeywordsRegExString);
             MatchCollection matchCollRTF = regex.Matches(rtfText.ToString());
-            System.Windows.Forms.RichTextBox richTxtBox = new System.Windows.Forms.RichTextBox();
-            richTxtBox.Rtf = rtfText;
-            MatchCollection matchCollTXT = regex.Matches(richTxtBox.Text);
 
-            List<string> plains = new List<string>();
-            foreach (Match m in matchCollTXT)
-                plains.Add(m.ToString().Replace('“', '"').Replace('”', '"'));
-
-            if (matchCollRTF.Count != matchCollTXT.Count)
-                throw new Exception("Parsing error");
-
-            for (int i = 0; i < matchCollTXT.Count; i++)
+            using (System.Windows.Forms.RichTextBox richTxtBox = new System.Windows.Forms.RichTextBox())
             {
-                rtfText = rtfText.Replace(matchCollRTF[i].ToString(), plains[i].ToString());
+                richTxtBox.Rtf = rtfText;
+                MatchCollection matchCollTXT = regex.Matches(richTxtBox.Text);
+
+
+                List<string> plains = new List<string>();
+                foreach (Match m in matchCollTXT)
+                    plains.Add(m.ToString().Replace('“', '"').Replace('”', '"'));
+
+                if (matchCollRTF.Count != matchCollTXT.Count)
+                    throw new Exception("Parsing error");
+
+                for (int i = 0; i < matchCollTXT.Count; i++)
+                {
+                    rtfText = rtfText.Replace(matchCollRTF[i].ToString(), plains[i].ToString());
+                }
             }
 
             return rtfText;
